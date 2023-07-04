0e9494f456f5f1ebb0acbdb25228f8ba9518eed6
Max <0665232@mail.ru>
2023-07-04 21:49:55 +0300

    '

---
+++ task01.java
@@ -0,0 +1,28 @@
+/*/
+1. Реализуйте 3 метода, чтобы в каждом из них получить разные исключения
+/*/
+
+public class task01 {
+
+    public static void main(String[] args) {
+        int[] arr = { 0, 1, 2 };
+
+        System.out.println(div(5, 0));              // ArithmeticException
+        System.out.println(parseInt("O"));          // NumberFormatException
+        System.out.println(getElement(arr, 5));   // ArrayIndexOutOfBoundsException
+
+    }
+
+    public static int div(int a, int b) {
+        return a / b;
+    }
+
+    public static int parseInt(String str) {
+        return Integer.parseInt(str);
+    }
+
+    public static int getElement(int[] array, int index) {
+        return array[index];
+    }
+
+}
